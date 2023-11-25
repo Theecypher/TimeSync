f52b789996747efcb7b62de9d63503fa3c163c85
Theecypher <obioramma12@gmail.com>
2023-11-25 09:22:08 +0100

    new changes

--- src/Pages/Onboarding/ResetPassword/ResetCode.jsx
+++ src/Pages/Onboarding/ResetPassword/ResetCode.jsx
@@ -1,27 +1,47 @@
-const CreatePassword = () => {
+import { Link } from "react-router-dom";
+
+const ResetCode = () => {
     return (
-        <section className="flex text-center items-center justify-center bg-blue-300 h-screen">
-            <div className="w-[300px] px-3 h-[380px] md:w-[400px] border-[1.7px] rounded-lg border-gray border-gray-200/60 bg-white">
-                <div className="py-3">
-                    <h2 className="py-[10px]">Logo</h2>
-                    <h2 className="font-bold text-[25px] md:text-[28px]">Reset code sent</h2>
-                    <p className="text-[14px] text-[#98a2b3]">
-                        If we find an account associated with that email, you'll
-                        receive an email, you'll receive a 4-didgit code. Please
-                        enter the code below
+        <section className="flex text-center lg:items-center lg:justify-center font-monserrat lg:bg-blue-300 h-screen  lg:backdrop-blur-[30px] lg:bg-[rgba(255, 255, 255, 0.2)]">
+            <form
+            className="w-full lg:w-[451px] mt-[48px] lg:mt-0 bg-white lg:border-[#F5F5F5] lg:shadow-md lg:border-[0.5px] lg:rounded-[12px] items-center justify-between lg:justify-start  lg:py-[20px] xl:py-[30px] 2xl:py-[48px] lg:px-[60px]"
+            >
+                <div className="pt-5 h-[140px]">
+                    <h2 className="py-[10px] hidden lg:block">Logo</h2>
+                    <h2 className="font-bold font-montserrat text-[25px] md:text-[28px] pb-2 leading-[35.2px]">Reset code sent</h2>
+                    <p className="text-[10px] pt-3 font-montserrat w-[38%] lg:w-[89%] lg:text-[14px] mx-auto text-[#98a2b3]">
+                        If we find an account associated with that email, 
+                        you will receive a 4-digit code. Please
+                        enter the code below.
 
                     </p>
                 </div>
 
-                <form className="h-52 w-full p-4 space-y-4">
+                <div className="mt-10 pt-5 ">
                     <input type="text"
-                    className="w-[50px] rounded py-2 border-[1.8px] border-black/20 m-3"
+                    className="w-[51px] rounded py-[10px] border-[1.8px] border-black/20 m-[3px] lg:m-[7px]"
                      />
-                </form>
+                    <input type="text"
+                    className="w-[51px] rounded py-[10px] border-[1.8px] border-black/20 m-[3px] lg:m-[7px]"
+                     />
+                    <input type="text"
+                    className="w-[51px] rounded py-[10px] border-[1.8px] border-black/20 m-[3px] lg:m-[7px]"
+                     />
+                    <input type="text"
+                    className="w-[51px] rounded py-[10px] border-[1.8px] border-black/20 m-[3px] lg:m-[7px]"
+                     />
+                   
+                   
+                </div>
 
-            </div>
+                <div className="flex justify-center m-7">
+                  <p className="mr-1 font-montserrat">Code will expire in 10:00</p>
+                    <Link className="font-montserrat underline lg:text-green-400 lg:no-underline text-blue-600">Resend code</Link>
+                </div>
+
+            </form>
         </section>
      );
 }
  
-export default CreatePassword;
+export default ResetCode;
