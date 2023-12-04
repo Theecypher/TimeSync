6faad94d6dc7fc3fb73d9c89c582c90f45c91180
EAconcepts <amooemmanuel10@gmail.com>
2023-12-04 22:20:40 +0100

    dashboard setup

--- src/Pages/dashboard/layout/SideMenu.jsx
+++ src/Pages/dashboard/layout/SideMenu.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SideMenu = () => {
   return (
-    <div className='w-[232px] flex flex-col h-screen border '> 
-      SideMenu
-      <Link to ='/dashboard'>Dashboard</Link>
-      <Link to ='time-tracker'>Time Tracker</Link>
+    <div className="w-[232px] flex flex-col h-screen border ">
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="time-tracker">Time Tracker</Link>
     </div>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
