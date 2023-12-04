7385f9571e5334bc0ddd502575b853e8c26e9df3
EAconcepts <amooemmanuel10@gmail.com>
2023-12-04 22:05:45 +0100

    set up the dashboard feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import SignIn from "./Pages/Onboarding/signIn/SignIn";
 import CreatePassword from "./Pages/Onboarding/ResetPassword/CreatePassword";
 import SignUp from "./Pages/Onboarding/sign-up/SignUp";
 import ResetCode from "./Pages/Onboarding/ResetPassword/ResetCode";
+import DashboardLayout from "./Pages/dashboard/layout/DashboardLayout";
+import TimeTracker from "./Pages/dashboard/TimeTracker";
+import Dashboard from "./Pages/dashboard/dashboard";
 
 function App() {
   return (
-    < div className=' font-montserrat'>
+    <div className=" font-montserrat">
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
         <Route path="/createPassword" element={<CreatePassword />} />
         <Route path="/resetCode" element={<ResetCode />} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="time-tracker" element={<TimeTracker />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/dashboard/layout/SideMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const SideMenu = () => {
+  return (
+    <div className='w-[232px] flex flex-col h-screen border '> 
+      SideMenu
+      <Link to ='/dashboard'>Dashboard</Link>
+      <Link to ='time-tracker'>Time Tracker</Link>
+    </div>
+  )
+}
+
+export default SideMenu
