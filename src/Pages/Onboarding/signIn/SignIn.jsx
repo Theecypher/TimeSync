cd09fbcf67dd3556d802be4cc7dccff2924ac24a
CodewithOyinda <honeybee08186@gmail.com>
2023-12-06 03:12:18 +0100

    SideMenu in progress

--- src/Pages/Onboarding/signIn/SignIn.jsx
+++ src/Pages/Onboarding/signIn/SignIn.jsx
@@ -42,8 +42,8 @@ const SignIn = () => {
     <div className='flex flex-col justify-center h-screen items-center sm:bg-blue-200'>
       <div className='w-[390px] h-[844px] px-3 flex flex-col justify-between items-center bg-white sm:w-[451px] sm:h-[525px] sm:rounded-lg sm:border-2 sm:shadow-lg sm:shadow-grey-200/60'>
         <div className='flex flex-col items-center leading-tight'>
-          <h2 className='font-bold py-10 text-xl'>Sign in</h2>
-          <p className='text-gray-400'>Sign in to continue working.</p>
+          <h2 className='font-montserrat font-bold py-10 text-[#1E1E1E] text-[20px] lg:text-[24px] '>Sign in</h2>
+          <p className='font-[400] font-montserrat text-[#1E1E1E] text-[10px] lg:text-[14px]'>Sign in to continue working.</p>
         </div>
         <form className='h-52 w-full p-4 space-y-4'>
           <input
@@ -72,13 +72,13 @@ const SignIn = () => {
         <div className='flex flex-col items-center leading-tight'>
           <button
             type='submit'
-            className='w-[331px] h-12 bg-blue-800 text-white rounded-2xl mx-2 sm:mb-1'
+            className='w-[331px] h-12 text-[#FFFFFF] bg-[#034592] font-[500] text-[16px] rounded-[16px] mx-2 sm:mb-1'
             onClick={handleSignIn}
           >
             Sign in
           </button>
-          <p className='w-full py-5 text-center'>
-            Don't have an account? <Link to='/' className='text-blue-600'>Create an account</Link>
+          <p className='w-full py-5 text-center text-[#737373] text-[12px] font-[400] '>
+            Don't have an account? <Link to='/' className='text-[14px] text-[#034592] underline ml-1'>Create an account</Link>
           </p>
         </div>
       </div>
